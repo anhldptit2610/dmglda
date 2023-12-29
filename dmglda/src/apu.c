ddd8bc3dd4d9a207846f1f025ecd7e2a67c426b4
anhldptit2610 <anhldptit.2610@gmail.com>
2023-12-29 22:48:25 +0700

    create APU module

---
+++ dmglda/src/apu.c
@@ -0,0 +1,10 @@
+/* For any x in 1, 2, 3, 4 
+   NRx0 - some channel-specific features(if present),
+   NRx1 - controls length timer
+   NRx2 - controls the volume and envelope
+   NRx3 - controls the period (maybe only partially)
+   NRx4 - has the channel's trigger & length timer enable bits,
+          as well as any leftover bits of period
+   
+   There are still some exceptions
+*/
